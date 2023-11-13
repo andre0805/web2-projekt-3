0310794c7938b39dd3d9e3cf12fee91f22bd57fe
Andre Flego <ade.flego@gmail.com>
2023-11-13 21:32:41 +0100

    resize game canvas on window resize

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ window.onblur = () => {
     game.resetPressedKeys();
 }
 
+// Resize the canvas if the window is resized
+window.onresize = () => {
+    game.canvas.width = window.innerWidth;
+    game.canvas.height = window.innerHeight;
+    game.player.x = game.canvas.width / 2;
+    game.player.y = game.canvas.height / 2;
+}
+
 (function () {
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
     window.requestAnimationFrame = requestAnimationFrame;
